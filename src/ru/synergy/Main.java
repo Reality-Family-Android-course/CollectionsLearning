988521a09c7580923c1be02bfbe42374d0955936
artemmad <artemmakarevich1997@gmail.com>
2021-07-04 17:38:18 +0300

    LinkedList bio example

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -2,45 +2,63 @@ package ru.synergy;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-     Cat[] cats  = new Cat[4];
-     cats[0] = new Cat("Томас");
-     cats[1] = new Cat("Бегемот");
-     cats[2] = new Cat("Филип Маркович");
-     cats[3] = new Cat("Котяра");
+        //Arrays
+        Cat[] cats = new Cat[4];
+        cats[0] = new Cat("Томас");
+        cats[1] = new Cat("Бегемот");
+        cats[2] = new Cat("Филип Маркович");
+        cats[3] = new Cat("Котяра");
 
-     //cats[1] = null;
+        //cats[1] = null;
 
-     System.out.println(Arrays.toString(cats));
+        System.out.println(Arrays.toString(cats));
 
-     ArrayList<Cat> catsList = new ArrayList();
-     for(Cat cat : cats){
-         catsList.add(cat);
-     }
+        // ArrayList
+        ArrayList<Cat> catsList = new ArrayList();
+        for (Cat cat : cats) {
+            catsList.add(cat);
+        }
 
-     catsList.add( new Cat("Бегемот2"));
+        catsList.add(new Cat("Бегемот2"));
 
-     catsList.remove(1);
+        catsList.remove(1);
 
-     Cat cat = catsList.get(0);
-     System.out.println(cat);
+        Cat cat = catsList.get(0);
+        System.out.println(cat);
 
-     System.out.println(catsList.indexOf(cat));
+        System.out.println(catsList.indexOf(cat));
 
-     catsList.add(2, cat);
-     catsList.set(2, new Cat("Меня сюда вставили"));
+        catsList.add(2, cat);
+        catsList.set(2, new Cat("Меня сюда вставили"));
 
-     catsList.removeAll(Arrays.asList(cat, catsList.get(3)));
+        catsList.removeAll(Arrays.asList(cat, catsList.get(3)));
 
-     System.out.println(catsList.toString());
+        System.out.println(catsList.toString());
 
         System.out.println(catsList.size());
+
+        //LinkedList
+        String str1 = new String("Hello world");
+        String str2 = new String("My name is Artem");
+        String str3 = new String("I love Java");
+
+        LinkedList<String> artemBio = new LinkedList<>();
+        artemBio.add(str1);
+        artemBio.add(str2);
+        artemBio.add(str3);
+
+
+        System.out.println(artemBio);
+
+        artemBio.remove(1);
+        System.out.println(artemBio);
 
     }
 }
